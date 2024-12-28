c5cde76e37cc9671e560f9eed358e6892b1bf2f2
Siddharth4590 <siddharthjagt253@gmail.com>
2024-12-28 23:22:36 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
         // Print "Hello, World!" to the console
-        System.out.println("Hello, World!");
+       
+         System.out.println("Hello, World!");
+         System.out.println("Hello, World!");
     }
 }
